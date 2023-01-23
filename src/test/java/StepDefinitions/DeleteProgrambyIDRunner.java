5fe1d1175397c4d20c71b30f459461664dbd6e95
dpoor <dpoor@192.168.2.13>
2023-01-23 12:18:18 -0500

    Commit BDD Project

--- src/test/java/StepDefinitions/DeleteProgrambyIDRunner.java
+++ src/test/java/StepDefinitions/DeleteProgrambyIDRunner.java
@@ -7,13 +7,14 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/test/resources/Features/DeleteProgram.feature",
+		//features="src/test/resources/Features/DeleteProgram.feature",
+		features="src/test/resources/Features",
 		glue="StepDefinitions",
 		monochrome=true,
 		plugin = {"summary", "pretty",
 		"html:target/HTMLReports/htmlreports.html",
 		"json:target/JSONReports/reports.json",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+		//"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 		}
 		)
  
